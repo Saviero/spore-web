bef5363ac03058ba1e95c1aba821d8f90cafa226
lizmoscow <elisaveta.moskovskaya@yandex.ru>
2017-11-29 03:22:17 +0300

    New parser for input data with tests

--- logs/parser.py
+++ logs/parser.py
@@ -1,55 +1,64 @@
-from decimal import Decimal
-import math
+import re
 
 from django.db import connection
 
-from .models import Result
 
 def isdecimal(str):
-    parts = str.split('.', maxsplit=1)
+    parts = str.split('.')
     if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit():
         return True
     return False
 
+
 def isfloat(str):
-    parts = str.split('e+', maxsplit=1)
+    parts = str.split('e+')
     if len(parts) == 1:
-        parts = str.split('e-', maxsplit=1)
+        parts = str.split('e-')
     if len(parts) == 2 and isdecimal(parts[0]) and parts[1].isdigit():
         return True
     return False
+
 
 def check_type(str):
     if str.isdigit():
         return 'INTEGER'
     if isdecimal(str):
-        precision1 = len(str.split('.')[0]) + 3
-        precision2 = len(str.split('.')[1]) + 3
-        return 'DECIMAL({},{})'.format(precision1, precision2)
+        return 'DECIMAL'
     if isfloat(str):
         return 'FLOAT'
-    return 'VARCHAR(max)'
+    return 'VARCHAR'
+
 
 def make_insert(str, template, jobname):
+    #Creates an SQL query, which inserts row of values into the table
+
     insert_string = "INSERT INTO " + jobname + "("
-    for i in range(len(template)):
-        insert_string += template[i][0] + ", "
-    insert_string += ") VALUES ("
+    for t in template[:-1]:
+        insert_string += t[0] + ", "    #adds columns names
+    insert_string += template[-1][0] + ") VALUES ("
     data = str.split()
-    for i, d in enumerate(data):
-        if template[i][1] == "VARCHAR(max)":
+
+    #if VARCHAR value is added, surrounds it with quotes
+    #else lives it unchanged
+    for i, d in enumerate(data[:-1]):
+        if re.match(r'VARCHAR', template[i][1]):
             insert_string += "'" + d + "', "
         else:
             insert_string += d + ", "
-    insert_string += ")"
+    if re.match(r'VARCHAR', template[-1][1]):
+        insert_string += "'" + data[-1] + "')"
+    else:
+        insert_string += data[-1] + ")"
     return insert_string
+
 
 def parse(filename, jobname):
     with open(filename, mode='r') as file:
         lines = file.readlines()
 
+        #checks if the syntax is correct
         if lines[0][0] == '#':
-            columns = lines[0][1:].strip('[]').split('][')
+            columns = lines[0][1:].strip('[]\n').split('][')
         else:
             raise SyntaxError
 
@@ -58,13 +67,43 @@ def parse(filename, jobname):
         for v in values:
             types.append(check_type(v))
 
+        #creates a list, containing max length for VARCHAR values and max precisions for DECIMAL ones
+        max_length = []
+        for t in types:
+            if t == 'VARCHAR':
+                max_length.append(0)
+            elif t == 'DECIMAL':
+                max_length.append([0,0])
+            else:
+                max_length.append(None)
+
+        #finds max length and precisions
+        for line in lines[1:]:
+            values = line.split()
+            for i, v in enumerate(values):
+                if type(max_length[i]) is int and max_length[i] < len(v):
+                    max_length[i] = len(v)
+                elif type(max_length[i]) is list and max_length[i][0] < len(v.split('.')[0]):
+                    max_length[i][0] = len(v.split('.')[0])
+                elif type(max_length[i]) is list and max_length[i][1] < len(v.split('.')[1]):
+                    max_length[i][1] = len(v.split('.')[1])
+
+        #adds obtained max length and precisions to type declarations
+        for i, m in enumerate(max_length):
+            if type(m) is int:
+                types[i] += '(' + str(m) + ')'
+            elif type(m) is list:
+                types[i] += '(' + str(m[0]) + ',' + str(m[1]) + ')'
+
         template = zip(columns, types)
 
         with connection.cursor() as cursor:
+            #concatenates an SQL query for table creation
             init_string = 'CREATE TABLE ' + jobname + ' ('
-            for pair in template:
-                init_string += pair[0] + ' ' + pair[2] + ', '
-            init_string += ')'
+            for pair in template[:-1]:
+                init_string += pair[0] + ' ' + pair[1] + ', '
+            init_string += template[-1][0] + ' ' + template[-1][1] + ')'
             cursor.execute(init_string)
-            for i in range(1, len(lines)):
-                cursor.execute(make_insert(lines[i], template, jobname))
+            # adds rows to the table
+            for line in lines[1:]:
+                cursor.execute(make_insert(line, template, jobname))
--- logs/tests.py
+++ logs/tests.py
@@ -25,24 +25,23 @@ class CheckTypeTest(TestCase):
     def test_with_string(self):
         str = 'R_0.17_N_1024_K_171.xpec'
         res = check_type(str)
-        self.assertEqual(res, 'VARCHAR(max)')
+        self.assertEqual(res, 'VARCHAR')
 
     def test_with_empty_string(self):
         str = ''
         res = check_type(str)
-        self.assertEqual(res, 'VARCHAR(max)')
+        self.assertEqual(res, 'VARCHAR')
 
 
 class MakeInsertTest(TestCase):
     def test_with_string(self):
         with connection.cursor() as cursor:
-            template = [('spec', 'VARCHAR(max)'), ('snr', 'DECIMAL(4,4)'), ('schedule', 'INTEGER'), ('fer', 'FLOAT')]
-            str = 'R_0.17_N_1024_K_171.xpec 0.0 2 1.31062e-001'
-            cursor.execute('CREATE TABLE test (spec VARCHAR(max), snr DECIMAL(4,4), schedule INTEGER, fer FLOAT)')
-            cursor.execute(make_insert(str, template, 'test'))
-            cursor.execute('SELECT spec, snr, schedule, fer FROM test')
-            res = cursor.fetchall()
-            expected_res = ('R_0.17_N_1024_K_171.xpec', Decimal(0.0), 2, 1.31062e-001)
+            template = [('spec', 'VARCHAR(25)'), ('snr', 'DECIMAL(1,2)'), ('schedule', 'INTEGER'), ('fer', 'FLOAT')]
+            jobname = 'test'
+            str = 'R_0.17_N_1024_K_171.xpec 0.25 2 1.31062e-001'
+            res = make_insert(str, template, jobname)
+            expected_res = "INSERT INTO test(spec, snr, schedule, fer) " \
+                           "VALUES ('R_0.17_N_1024_K_171.xpec', 0.25, 2, 1.31062e-001)"
             self.assertEqual(res, expected_res)
 
 
@@ -52,20 +51,20 @@ class ParserTest(TestCase):
         jobname = 'test'
         file = open(filename, mode='w')
         file.write('#[spec][snr][schedule][N][K][R][stddev][fer]\n')
-        file.write('R_0.17_N_1024_K_171.xpec 0.0 2 1024 171 0.17 1.73036e+000 1.31062e-001\n')
-        file.write('R_0.17_N_1024_K_171.xpec 0.0 3 1024 171 0.17 1.73036e+000 1.31062e-001\n')
+        file.write('R_0.17_N_1024_K_171 0.0 2 1024 171 56.17 1.73036e+000 1.31062e-001\n')
+        file.write('R_0.17_N_1024_K_171.xpec 0.25 3 1024 171 4.17 1.73036e+000 1.31062e-001\n')
         file.close()
-        answer = parse(filename, jobname)
-        self.assertEqual(type(answer), 'NoneType')
+        parse(filename, jobname)
 
-        with connection.cursor as cursor:
+        with connection.cursor() as cursor:
             cursor.execute('SELECT spec, snr, schedule, N, K, R, stddev, fer FROM test WHERE schedule = 2')
             res1 = cursor.fetchall()
-            expected_res1 = ('R_0.17_N_1024_K_171.xpec', Decimal(0.0), 2, 1024, 171, Decimal(0.17), 1.73036e+000, 1.31062e-001)
+            expected_res1 = [(u'R_0.17_N_1024_K_171', Decimal('0'), 2,
+                              1024, 171, Decimal('56.17'), 1.73036, 0.131062)]
             self.assertEqual(res1, expected_res1)
             cursor.execute('SELECT spec, snr, schedule, N, K, R, stddev, fer FROM test WHERE schedule = 3')
             res1 = cursor.fetchall()
-            expected_res1 = (
-            'R_0.17_N_1024_K_171.xpec', Decimal(0.0), 3, 1024, 171, Decimal(0.17), 1.73036e+000, 1.31062e-001)
+            expected_res1 = [(u'R_0.17_N_1024_K_171.xpec', Decimal('0.25'), 3,
+                              1024, 171, Decimal('4.17'), 1.73036, 0.131062)]
             self.assertEqual(res1, expected_res1)
         os.remove(filename)
