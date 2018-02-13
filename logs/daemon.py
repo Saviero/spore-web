7a4d638d37f74d3a85d0b31a9aa6c2e12fa1d8e8
lizmoscow <elisaveta.moskovskaya@yandex.ru>
2018-02-13 17:31:07 +0300

    an attempt to make a daemon

--- logs/daemon.py
+++ logs/daemon.py
@@ -1,11 +1,19 @@
-import threading
-import time
+from htcondor import Schedd
+from classad import ExprTree
 
-def daemon():
-    time.sleep(60)
+from sim.models import JobIdModel
+from models import FinishedJobs
+from parser import parse
 
+def check_history():
+    schedd = Schedd()
+    jobs = list(schedd.history(ExprTree(''), ["ClusterId"], -1))
 
-
-_thread = threading.Thread(target=daemon)
-_thread.setDaemon(True)
-_thread.start()
+    for job in jobs:
+        job_by_id = JobIdModel.objects.all(cluster_id=job["ClusterId"])
+        if job_by_id.exists():
+            filename = '/{0}/out'.format(job_by_id.job_name)
+            parse(filename, job_by_id.job_name)
+            finished_job = FinishedJobs(job_name=job_by_id.job_name, cluster_id=job_by_id.cluster_id)
+            finished_job.save()
+            job_by_id.delete()
